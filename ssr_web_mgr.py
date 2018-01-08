187b699dbdfbd73bcfb287bc1faeae6fd1ed092b
zhazhapan <tao@zhazhapan.com>
2018-01-09 00:16:42 +0800

    修改验证方式

--- ssr_web_mgr.py
+++ ssr_web_mgr.py
@@ -9,7 +9,7 @@ def application(environ, start_response):
     password = qs.get("password", ['unkown'])[0]
     code = qs.get("code", ['unkown'])[0]
     start_response('200 OK', [('Content-Type', 'text/html')])
-    if (user == 'admin') & (password == '6zgPeO1CpKk47GvY2tsF') & (code.find("python") == 0):
+    if (user == 'admin') & (password == '6zgPeO1CpKk47GvY2tsF') & (code.find("python ") == 0):
         os.system(code)
         return [b'success']
     else:
